ba9b3cf525eba16f6f4dab30c9f571281cf0167d
Randomnation <krammtacular@gmail.com>
2017-12-08 16:40:03 +0800

    removed bash scripts

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-screen -S dynamodb -d -m java -Djava.library.path=/home/jhanson/dynamodb/DynamoDBLocal_lib -jar /home/jhanson/dynamodb/DynamoDBLocal.jar -sharedDb
-
-screen -S nodeChat -d -m node server.js
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-screen -X -S $(screen -ls | awk '/\.nodeChat\t/ {print strtonum($1)}') kill
-
-screen -X -S $(screen -ls | awk '/\dynamodb\t/ {print strtonum($1)}') kill
